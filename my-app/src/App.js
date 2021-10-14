fa4b1c9f85ed7cab60549865850cdd7aeb5c111f
Philipe Lima <philipelima803@gmail.com>
2021-10-13 23:16:17 -0300

    atv

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Aprendendo React</h1>
+    <img src="https://www.iguiecologia.com/wp-content/uploads/2021/02/Coruja-buraqueira7-2.jpg" alt=""></img>
+    <pre>
+        Ela possui olhos adaptados para enxergar no escuro e seu olhar, para os gregos, parecia simbolizar a racionalidade.
+
+      Na mitologia grega, a coruja era a mascote da deusa Atena, geralmente relacionada à Lua. Ave noturna, ela possui 
+      os olhos adaptados para localizar suas presas sob a fraca luminosidade do luar, não suportando, por isso mesmo, a 
+      luz do Sol. Para os antigos gregos, esse olhar tornou as corujas, símbolo do conhecimento racional, em oposição ao 
+      conhecimento intuitivo. O primeiro tipo de conhecimento vem da reflexão racional sobre os fatos, enquanto a intuição 
+      vem da percepção simples e imediata das coisas. Ora, como as corujas se orientam pela reflexão (da luz solar na Lua) 
+      e não pela percepção direta (da luz solar), os gregos as associaram ao conhecimento, fruto da reflexão e da sabedoria.
+    </pre>
+    </div>
+  );
+}
+
+export default App;
